b484233e10e965d4d602167a6e782f6ce8a983fc
Ish Kumar <Ish.Kumar@Geminisolutions.com>
2023-09-19 11:38:38 +0530

    Section

---
+++ controllers/Subsection.js
@@ -0,0 +1,18 @@
+const Subsection  =  require("..models/SubSection");
+const Section = require("../models/Section");
+
+// create SubSection
+
+exports.createSubSection = async (req,res)=>{
+    try{
+        // fetch data from req body
+        // extract file/video
+        // validation
+        // upload video to cloudinary
+        // create section with this sub section ObjectId
+        // return response
+    }
+    catch(error){
+
+    }
+};
